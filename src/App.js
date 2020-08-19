320b1a6b1f66970164cd38868c80eefa35b5df65
Monica <maguado85@gmail.com>
2020-08-19 20:14:37 +0200

    NEW: WIP install Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import getData from './api/Api';
+import Home from './components/Home';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 export default class App extends Component {
 
@@ -26,10 +28,25 @@ export default class App extends Component {
     console.log(this.state.womenList)
 
     const { womenList } = this.state;
-    
+
     return(
-      <div className="App">
-        {womenList}
+      <div className="root">
+        <div className="main_app">
+        <h1 clasName="title">muSHEc</h1>
+
+
+        <Switch>
+          <Route
+            path="/">
+            <Home
+              womenList={womenList}>
+
+            </Home>
+
+          </Route>
+        </Switch>
+
+        </div>
       </div>
 
     )
